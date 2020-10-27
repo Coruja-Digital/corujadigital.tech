57ced61812eaff17eae1e129460cce24dc0d06a6
ivanhercaz <ivan@ivanhercaz.com>
2020-10-27 18:39:29 +0000

    cookies: añadir script para el aviso

---
+++ output/theme/js/cookieconsent-init.js
@@ -0,0 +1,17 @@
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#252e39"
+    },
+    "button": {
+      "background": "#14a7d0"
+    }
+  },
+  "theme": "edgeless",
+  "content": {
+    "message": "Este sitio web utiliza cookies, aunque intentamos no utilizarlas. Puedes leer más sobre estas cookies y desactivarlas en nuestra política de privacidad.",
+    "dismiss": "¡Entendido!",
+    "link": "Política de privacidad.",
+    "href": "/politica-privacidad"
+  }
+});
